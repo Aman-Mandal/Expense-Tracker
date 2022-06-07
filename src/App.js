402d77a2ccc910c53c9de4d56e08a7a74bb78340
Aman-Mandal <002amankumar@gmail.com>
2022-06-07 15:02:13 +0530

    Adding expenses dynamically

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import NewExpense from './components/NewExpense/NewExpense'
 import Expenses from './components/Expenses/Expenses'
-import React from 'react'
+import React, { useState } from 'react'
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Electricity Bill',
+    amount: 94.12,
+    date: new Date(2022, 5, 5),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'Laptop',
+    amount: 450,
+    date: new Date(2022, 4, 12),
+  },
+]
 
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Electricity Bill',
-      amount: 94.12,
-      date: new Date(2022, 5, 5),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2022, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'Laptop',
-      amount: 450,
-      date: new Date(2022, 4, 12),
-    },
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
+  useState()
   // Children-Parent Communication
   const addExpenseHandler = expense => {
-    console.log('In App.JS')
-    console.log(expense)
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    })
   }
 
   return (
