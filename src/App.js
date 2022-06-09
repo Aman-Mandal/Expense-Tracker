bd99a62eb246455229710d3619f08ebbc9b9b1e9
Aman-Mandal <002amankumar@gmail.com>
2022-06-09 23:00:46 +0530

    Html formatted

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const DUMMY_EXPENSES = [
     amount: 94.12,
     date: new Date(2021, 5, 5),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 2, 12) },
+  { id: 'e2', title: 'New TV', amount: 499.49, date: new Date(2022, 2, 12) },
   {
     id: 'e3',
     title: 'Car Insurance',
